be2087940bb282b09685b3892052c5806bcecde4
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-07-18 09:51:34 +0500

    ss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import "./styles/index.css";
 
 render(
   <React.StrictMode>
